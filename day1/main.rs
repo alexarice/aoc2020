413b1919744e0bdbc50a4bedb51233771bd3e48a
Alex Rice <alexrice999@hotmail.co.uk>
2020-12-06 11:35:37 +0000

    Make my own iterator for 1

--- day1/main.rs
+++ day1/main.rs
@@ -1,28 +1,45 @@
 use std::fs;
 
+struct MyIter<I> {
+    iter : I,
+}
+impl<I> Iterator for MyIter<I>
+where I: Iterator,
+      I: Clone,
+{
+    type Item = (<I as Iterator>::Item,I);
+
+    fn next(&mut self) -> Option<Self::Item> {
+	let i = self.iter.next();
+	i.and_then(|x| Some((x, self.iter.clone())))
+    }
+}
+impl<I> MyIter<I> {
+    fn new(iter : I) -> MyIter<I> {
+	MyIter { iter }
+    }
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt")
 	.expect("Something went wrong reading the file");
 
     let numbers : Vec<i32> = input.split("\n").filter(|x| x.len() > 0).map(|x| x.parse().expect("Parsing failed")).collect();
 
-    let len = numbers.len();
-
-    for i in 0 .. len {
-	for j in i + 1 .. len {
-	    let (ni,nj) = (numbers[i],numbers[j]);
-	    if ni + nj == 2020 {
-		println!("{} * {} = {}",ni,nj,ni*nj);
+    let iter = || MyIter::new(numbers.iter());
+    for (i , rest) in iter() {
+	for j in rest {
+	    if i + j == 2020 {
+		println!("{} * {} = {}",i,j,i*j);
 	    }
 	}
     }
 
-    for i in 0 .. len {
-	for j in i + 1 .. len {
-	    for k in j + 1 .. len {
-		let (ni, nj, nk) = (numbers[i],numbers[j],numbers[k]);
-		if ni + nj + nk == 2020 {
-		    println!("{} * {} * {} = {}",ni,nj,nk,ni*nj*nk);
+    for (i , rest) in iter() {
+	for (j , rest) in MyIter::new(rest) {
+	    for k in rest {
+		if i + j + k == 2020 {
+		    println!("{} * {} * {} = {}",i,j,k,i*j*k);
 		}
 	    }
 	}
