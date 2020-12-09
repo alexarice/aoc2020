1167d2dfad75d0d80b82ee134b37d44210d09c9d
Alex Rice <alexrice999@hotmail.co.uk>
2020-12-09 10:39:56 +0000

    Use itertools in day 1

--- day1/main.rs
+++ day1/main.rs
@@ -1,47 +1,21 @@
 use std::fs;
-
-struct MyIter<I> {
-    iter : I,
-}
-impl<I> Iterator for MyIter<I>
-where I: Iterator,
-      I: Clone,
-{
-    type Item = (<I as Iterator>::Item,I);
-
-    fn next(&mut self) -> Option<Self::Item> {
-	let i = self.iter.next();
-	i.and_then(|x| Some((x, self.iter.clone())))
-    }
-}
-impl<I> MyIter<I> {
-    fn new(iter : I) -> MyIter<I> {
-	MyIter { iter }
-    }
-}
+use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("input.txt")
 	.expect("Something went wrong reading the file");
 
-    let numbers : Vec<i32> = input.split("\n").filter(|x| x.len() > 0).map(|x| x.parse().expect("Parsing failed")).collect();
+    let numbers = input.lines().map(|x| x.parse::<u32>().expect("Parsing failed"));
 
-    let iter = || MyIter::new(numbers.iter());
-    for (i , rest) in iter() {
-	for j in rest {
-	    if i + j == 2020 {
-		println!("{} * {} = {}",i,j,i*j);
-	    }
+    for (i , j) in numbers.clone().tuple_combinations() {
+	if i + j == 2020 {
+	    println!("{} * {} = {}",i,j,i*j);
 	}
     }
 
-    for (i , rest) in iter() {
-	for (j , rest) in MyIter::new(rest) {
-	    for k in rest {
-		if i + j + k == 2020 {
-		    println!("{} * {} * {} = {}",i,j,k,i*j*k);
-		}
-	    }
+    for (i, j, k) in numbers.tuple_combinations() {
+	if i + j + k == 2020 {
+	    println!("{} * {} * {} = {}",i,j,k,i*j*k);
 	}
     }
 }
