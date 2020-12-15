37d2636544fe3f45a98cf8af74a539cb6282b63e
Alex Rice <alexrice999@hotmail.co.uk>
2020-12-15 10:18:45 +0000

    Day 15

---
+++ day15/main.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+fn getnth(num: usize) -> u32 {
+    let input = [6, 19, 0, 5, 7, 13, 1];
+    let mut memory: HashMap<u32, usize> = HashMap::new();
+
+    for (n, x) in input.iter().enumerate() {
+        memory.insert(*x, n + 1);
+    }
+
+    let mut turn = input.len();
+    let mut next = 0;
+
+    while turn < num - 1 {
+        turn += 1;
+        let last_used: Option<usize> = memory.get(&next).cloned();
+        memory.insert(next, turn);
+        match last_used {
+            None => {
+                next = 0;
+            }
+            Some(n) => {
+                next = turn as u32 - n as u32;
+            }
+        }
+        print!("{} ", next);
+        if turn % 20 == 0 {
+            println!("");
+        }
+    }
+    next
+}
+
+fn main() {
+    println!("Part 1: {}", getnth(2020));
+    println!("Part 2: {}", getnth(30000000));
+}
